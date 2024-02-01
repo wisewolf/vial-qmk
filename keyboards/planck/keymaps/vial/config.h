2e727db6c9b81a80720df0a53ddd8a85557cbe46
Nuno Miguel Correia <nunomccorreia@gmail.com>
2024-02-01 08:46:28 +0000

    Added support for VIAL

---
+++ keyboards/planck/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x8C, 0xBC, 0x66, 0xAF, 0xDF, 0x26, 0x57, 0x1A}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 2 }
